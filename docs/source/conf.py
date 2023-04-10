b29a58b219b8eb56151af131e00ddfa2c90e52a1
Porvatov Vadim <eighonet@gmail.com>
2023-04-10 20:06:56 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AMPy'
+copyright = '2023, Swarmtronics'
+author = 'Vadim Porvatov, Mikhail Buzakov'
 
 release = '0.1'
 version = '0.1.0'
